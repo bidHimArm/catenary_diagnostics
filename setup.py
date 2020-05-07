7a5342b859710b3147963e4a49a883bded5ec809
bidhimarm <mehdi.brahimi@outlook.com>
2020-05-07 22:24:07 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An approach for OCS (Overhead Catenary System) diagnostics based on the contact force measurements',
+    author='Femto-ST',
+    license='MIT',
+)
